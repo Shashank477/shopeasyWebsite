5af5596e6dc440ae09cdd225f203894b29d05007
Shashank <shashankmn171@gmail.com>
2025-11-27 17:05:23 +0530

    service worker added

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
 	// in your serviceworker.js file
 	// importScripts("<link-that-you-copied>");
 	// for eg, your dashboard host is dashboard-03.moengage.com
-	importScripts("https://cdn.moengage.com/release/dc_3/versions/2/serviceworker_cdn.min.latest.js?_gl=1*u1cw9f*_gcl_au*MTIwMDQ0MzM5Mi4xNzYyNTEzMTc2LjQzNDYyNTIwNC4xNzYzODQzOTc1LjE3NjM4NDM5NzQ.*_ga*MTE1ODAyNzM3My4xNzU0NDU1MzY5*_ga_SEBHW7YTZ7*czE3NjQyMTk4MDEkbzE2JGcxJHQxNzY0MjM4MzkyJGo1NSRsMCRoMA..");
+	importScripts("https://cdn.moengage.com/release/dc_3/versions/2/serviceworker_cdn.min.latest.js");
